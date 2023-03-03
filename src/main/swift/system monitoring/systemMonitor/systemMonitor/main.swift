16f3b96775dd89cf67f64feddd01538eadd7c70e
Иван <vania.gorshevhnikov@gmail.com>
2023-03-03 20:44:34 +0300

    Update swift module

--- src/main/swift/system monitoring/systemMonitor/systemMonitor/main.swift
+++ src/main/swift/system monitoring/systemMonitor/systemMonitor/main.swift
@@ -1,46 +1,92 @@
 import Foundation
 import PerfectHTTP
 import PerfectHTTPServer
+import ArgumentParser
+
+var isRun : Bool = false;
+var filePathGlobal: String = "";
+
+struct Parser: ParsableCommand {
+    @Argument() var filePath: String
+    
+    func run() {
+        filePathGlobal = filePath
+        startServer()
+    }
+}
 
 func startHandler(request: HTTPRequest, response: HTTPResponse) {
-    let queue = DispatchQueue.global(qos: .default)
-    queue.sync {
-        queue.async {
-            startMonitoring(interval: Int(request.queryParams[0].1) ?? 1)
+    if !isRun {
+        isRun = true
+        let queue = DispatchQueue.global(qos: .default)
+        queue.sync {
+            response.setHeader(.contentType, value: "json")
+            response.setHeader(HTTPResponseHeader.Name.accessControlAllowOrigin, value: "*")
+            do { try startMonitoring() }
+            catch {
+                response.completed(status: HTTPResponseStatus.internalServerError)
+                return
+            }
+            queue.async {
+                enterRecords(interval: Int(request.queryParams[0].1) ?? 1)
+            }
+            print("Start monitoring")
+            response.completed()
         }
-    response.setHeader(.contentType, value: "text/html")
-    response.appendBody(string: "<html><title>Hello, world!</title><body>start monitoring!</body></html>")
-    response.completed()
+    } else {
+        response.setHeader(.contentType, value: "json")
+        response.setHeader(HTTPResponseHeader.Name.accessControlAllowOrigin, value: "*")
+        response.completed(status: HTTPResponseStatus.badRequest)
     }
 }
 
 func endHandler(request: HTTPRequest, response: HTTPResponse) {
-    let queue = DispatchQueue.global(qos: .default)
-    queue.sync {
-        queue.async {
-            endMonitoring()
+    if isRun {
+        isRun = false
+        let queue = DispatchQueue.global(qos: .default)
+        queue.sync {
+            do { try endMonitoring() }
+            catch {
+                response.completed(status: HTTPResponseStatus.internalServerError)
+                return
+            }
+            response.setHeader(.contentType, value: "json")
+            response.setHeader(HTTPResponseHeader.Name.accessControlAllowOrigin, value: "*")
+            response.appendBody(string: "{\"text\": \"end\"}")
+            print("End monitoring")
+            response.completed()
         }
-    response.setHeader(.contentType, value: "text/html")
-    response.appendBody(string: "<html><title>Hello, world!</title><body>end monitoring!</body></html>")
-    response.completed()
+    } else {
+        response.setHeader(.contentType, value: "json")
+        response.setHeader(HTTPResponseHeader.Name.accessControlAllowOrigin, value: "*")
+        response.completed(status: HTTPResponseStatus.badRequest)
     }
 }
 
-func startServer()
-{
+
+func loadHandler(request: HTTPRequest, response: HTTPResponse) {
+    response.setHeader(.contentType, value: "json/application")
+    response.setHeader(HTTPResponseHeader.Name.accessControlAllowOrigin, value: "*")
+    let load = getLoad()
+    response.appendBody(string: "{\"CPU\":\(load.CPU), \"RAM\":\(load.RAM), \"Time\":\"\(load.Time)\"}")
+    print("get load")
+    response.completed()
+}
+
+func startServer() {
     do {
         var routes = Routes()
-        routes.add(method: .get, uri: "/monitoring/start", handler: startHandler)
-        routes.add(method: .get, uri: "/monitoring/end", handler: endHandler)
-        try HTTPServer.launch(name: "localhost",
+            routes.add(method: .get, uri: "/monitoring/get", handler: loadHandler)
+            routes.add(method: .get, uri: "/monitoring/start", handler: startHandler)
+            routes.add(method: .get, uri: "/monitoring/end", handler: endHandler)
+            try HTTPServer.launch(name: "localhost",
                           port: 8081,
                           routes: routes,
                           responseFilters: [
                           (PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)])
-    }
-    catch {
+    } catch {
         print(error)
     }
 }
 
-startServer()
+Parser.main()
--- src/main/swift/system monitoring/systemMonitor/systemMonitor/monitor.swift
+++ src/main/swift/system monitoring/systemMonitor/systemMonitor/monitor.swift
@@ -3,9 +3,15 @@ import Foundation
 import PostgresClientKit
 import SystemKit
 
+
+enum MyError: Error {
+  case InvalidConfig
+  case OutOfRange
+}
+
 let queue = DispatchQueue.global(qos: .default)
 
-var isRun : Bool = false;
+var sys = System()
 var idNode :Int = -1;
 
 func getMaxIdNode(connection : Connection, table: String, column: String) throws -> Int {
@@ -27,6 +33,12 @@ func openConnection(configPath : String) throws -> Connection {
     let fileContent = try? NSString(contentsOfFile: configPath, encoding: String.Encoding.utf8.rawValue)
 
     let value = try! Yaml.load(fileContent! as String)["database"]
+    
+    if ((value["host"] == nil) || value["port"] == nil || value["ssl"] == nil || value["name"] == nil || value["user"] == nil)
+    {
+        throw MyError.InvalidConfig
+    }
+    
     configuration.host = value["host"].string!
     configuration.port = value["port"].int!
     configuration.ssl = value["ssl"].bool!
@@ -51,45 +63,51 @@ func endRecord(connection:Connection, idNode: Int) throws {
 
 func enterRecords(connection : Connection, idNodes: Int, interval: Int) throws {
     var idNodeLoadDump = try! getMaxIdNode(connection: connection, table: "node_load_dump", column: "id_record") + 1
-    var sys = System()
     
-    while isRun {
-        let cpuUsage = sys.usageCPU()
-        let memoryUsage = System.memoryUsage()
-        let sql = "insert into node_load_dump (id_record, id_node, \"CPU_usage\", \"RAM_usage\", timestamp) values(\(idNodeLoadDump), \(idNodes), \(cpuUsage.user + cpuUsage.system), \(100 * (System.physicalMemory() - memoryUsage.inactive - memoryUsage.free)/System.physicalMemory()), '\(Date().postgresTimestampWithTimeZone)')"
-        let statement = try connection.prepareStatement(text: sql).execute()
-        defer { statement.close() }
-        idNodeLoadDump += 1
-        sleep(UInt32(interval))
-    }
+    let cpuUsage = sys.usageCPU()
+    let memoryUsage = System.memoryUsage()
+    let sql = "insert into node_load_dump (id_record, id_node, \"CPU_usage\", \"RAM_usage\", timestamp) values(\(idNodeLoadDump), \(idNodes), \(100 - cpuUsage.idle), \(100 * (System.physicalMemory() - memoryUsage.inactive - memoryUsage.free)/System.physicalMemory()), '\(Date().postgresTimestampWithTimeZone)')"
+    let statement = try connection.prepareStatement(text: sql).execute()
+    defer { statement.close() }
+    idNodeLoadDump += 1
+    sleep(UInt32(interval))
 }
 
-func startMonitoring(interval: Int) {
+struct Load {
+    let CPU : Double
+    let RAM : Double
+    let Time : PostgresTimestampWithTimeZone
+}
+
+func getLoad() -> Load {
+    let usageCPU = sys.usageCPU()
+    let memoryUsage = System.memoryUsage()
+    return Load(CPU: 100 - usageCPU.idle , RAM: 100 * (System.physicalMemory() - memoryUsage.inactive - memoryUsage.free)/System.physicalMemory(), Time: Date().postgresTimestampWithTimeZone)
+    
+}
+
+func startMonitoring() throws {
+    let connection = try openConnection(configPath: filePathGlobal + "/config/config.yaml")
+    defer {connection.close()}
+    idNode = try! getMaxIdNode(connection: connection, table: "nodes", column: "id_node") + 1
+
+    try startRecord(connection: connection, idNode: idNode)
+}
+
+
+func enterRecords(interval: Int) {
     do {
-        if !isRun {
-            isRun = true
-            let connection = try openConnection(configPath: "/Users/ivangorshechnikov/Xcode/Test/config.yaml")
-            defer {connection.close()}
-            idNode = try! getMaxIdNode(connection: connection, table: "nodes", column: "id_node") + 1
-            try startRecord(connection: connection, idNode: idNode)
-            try enterRecords(connection: connection, idNodes: idNode, interval: interval)
-        }
+        let connection = try openConnection(configPath: filePathGlobal + "/config/config.yaml")
+        defer {connection.close()}
+        try enterRecords(connection: connection, idNodes: idNode, interval: interval)
     }
     catch {
         print(error)
     }
 }
 
-func endMonitoring() {
-    do {
-        if isRun {
-            isRun = false
-            let connection = try openConnection(configPath: "/Users/ivangorshechnikov/Xcode/Test/config.yaml")
-            defer {connection.close()}
-            try endRecord(connection: connection, idNode: idNode)
-        }
-    }
-    catch {
-        print(error)
-    }
+func endMonitoring() throws {
+    let connection = try openConnection(configPath: filePathGlobal + "/config/config.yaml")
+    defer {connection.close()}
+    try endRecord(connection: connection, idNode: idNode)
 }
